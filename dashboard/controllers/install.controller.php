7bae181fed21520768dd1e3f1b0885eef8ad631e
DevOps PcNay <pcnay2003@outlook.com>
2025-05-01 20:24:13 -0700

    Se crea la tabla desde PHP, usando instruccione de SQL

--- dashboard/controllers/install.controller.php
+++ dashboard/controllers/install.controller.php
@@ -48,9 +48,34 @@
     {
       if (isset($_POST["email_admin"]))
       {
+        /*
         echo '<pre>Variables POST';print_r($_POST);echo'</pre>';
         echo '<pre>Conexion Base De Datos ';print_r(InstallController::connect());echo'</pre>';
-        
+        */
+        // Creando la tabla "admins"
+        $sqlAdmins = "CREATE TABLE admins (
+        id_admin INT NULL AUTO_INCREMENT,
+        rol_admin TEXT NULL DEFAULT NULL,
+        permissions_admin TEXT NULL DEFAULT NULL,
+        email_admin TEXT NULL DEFAULT NULL,
+        password_admin TEXT NULL DEFAULT NULL,
+        token_admin TEXT NULL DEFAULT NULL,
+        token_exp_admin TEXT NULL DEFAULT NULL,
+        status_admin INT NULL DEFAULT 1,
+        title_admin TEXT NULL DEFAULT NULL,
+        symbol_admin TEXT NULL DEFAULT NULL,
+        font_admin TEXT NULL DEFAULT NULL,
+        color_admin TEXT NULL DEFAULT NULL,
+        back_admin TEXT NULL DEFAULT NULL,
+        date_created_admn DATE NULL DEFAULT NULL,
+        date_updated_admin TIMESTAMP on update CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        PRIMARY KEY (id_admin)) ENGINE = InnoDB;";
+        $stmtAdmins = InstallController::connect()->prepare($sqlAdmins);
+
+        if ($stmtAdmins->execute())
+        {
+          echo '<script> alert("Tabla creada exitosamente")</script>';
+        }
       }
     }
 
